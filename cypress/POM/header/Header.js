b36ce62c872c2a9b22c1250d0c0a2ae0107f2c19
unknown <albert.galstyan01@gmail.com>
2024-09-27 09:57:14 +0400

    Added header POM-2

--- cypress/POM/header/Header.js
+++ cypress/POM/header/Header.js
@@ -1,6 +1,24 @@
 class Header{
 
     elements = {
+        home:() => {
+            return cy.get();
+        },
+        form:() =>{
+            return cy.get();
+        },
+        imageUpload:() => {
+            return cy.get();
+        },
+        table:() => {
+            return cy.get();
+        },
+        api:() => {
+            return cy.get();
+        },
+        eCommerce:() => {
+            return cy.get();
+        }
         
     }
 }
